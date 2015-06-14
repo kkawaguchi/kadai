328802773440ba1b049073aa5202faa0a0783c7c
kawaguchi <qqvs4bc9k@grace.ocn.ne.jp>
2015-06-14 23:20:44 +0900

    不要なクラスを削除しました

--- kadai10/kadai10.cs
+++ kadai10/kadai10.cs
@@ -3,79 +3,29 @@ using System.IO;
 
 namespace Kadai
 {
-//⑩Inputインターフェースの作成 　及び実装クラスTextInputクラスの作成 　・Inputインターフェース　　→　inputメソッドを持つインターフェース 　・TextInputクラス　　　　　→　inputメソッドでテキストファイルから文字列を取り込むクラス。
+//⑩Inputインターフェースの作成 　及び実装クラスTextInputクラスの作成 　・Inputインターフェース　　→　inputメソッドを持つインターフェース 　
+//・TextInputクラス　　　　　→　inputメソッドでテキストファイルから文字列を取り込むクラス。
+
 	class Kadai10
 	{
 
 		public static void Main()
 		{
-			ConsoleOutput HelloWorld = new ConsoleOutput();
-			HelloWorld.SetValue("Hello World");
-			HelloWorld.Output();
-			
-			TextOutput TextHelloWorld = new TextOutput();
-			TextHelloWorld.SetValue("Hello World");
-			TextHelloWorld.Output();
-			
-			TextInput FileContents = new TextInput();
+			IInput FileContents = new TextInput();
 			FileContents.Input();
 			Console.WriteLine(FileContents.GetValue());
 		}
 	}
 	
-	public interface Output
-	{
-		void Output();
-	}
-	
-	class ConsoleOutput:Output
-	{
-		protected string value;
-		
-		public string GetValue()
-		{
-			return value;
-		}
-		
-		public void SetValue(string outPutValue)
-		{
-			value = outPutValue;
-		}
-		
-		public void Output()
-		{
-			Console.WriteLine(value);
-		}
-	}
-	
-	class TextOutput:Output
-	{
-		protected string value;
-		
-		public string GetValue()
-		{
-			return value;
-		}
-		
-		public void SetValue(string outPutValue)
-		{
-			value = outPutValue;
-		}
-		public void Output()
-		{
-			File.WriteAllText("./kadai.txt",value);
-		}
-	}
-	
-	public interface Input
+	public interface IInput
 	{
 		void Input();
+		string GetValue();
 	}
 	
-	class TextInput:Input
+	class TextInput:IInput
 	{
 		protected string value;
-		protected string[] values;
 		
 		public string GetValue()
 		{
@@ -84,9 +34,7 @@ namespace Kadai
 		
 		public void Input()
 		{
-			values = File.ReadAllLines("./kadai.txt");
-			value = values[0];
+			value = File.ReadAllText("./kadai.txt");
 		}
 	}
-	
 }
