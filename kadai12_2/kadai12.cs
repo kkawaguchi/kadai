e1e45aa8c0019ee9a2a9d34ecb3da73f14153791
Kei Kawaguchi <k.kawaguchi@cisys.co.jp>
2015-06-18 21:32:52 +0900

    課題12をStream Reader とStream Writerを使って作成しました

---
+++ kadai12_2/kadai12.cs
@@ -0,0 +1,50 @@
+using System; 
+using System.IO; 
+
+namespace Kadai
+{
+	//⑫入出力機能をもつクラスを作成してください。
+
+	//以下のメソッドを実装していること
+	
+	// ①テキストファイルの内容を読み込み、 引数に指定されたパスが示すクラス内のフィールドに格納するメソッド public void input(String path);
+
+	// ②クラスのフィールドに格納された内容を、 引数に指定されたパスが示すテキストファイルに出力するメソッド public void output(String path);
+
+	class Kadai12
+	{
+		public static void Main()
+		{
+			InputOutput kadai12 = new InputOutput();
+			kadai12.Input("C:/川口/kadai/kadai12_2/test.txt");
+			kadai12.Output("C:/川口/kadai/kadai12_2/test2.txt");
+		}
+	}
+
+	class InputOutput
+	{
+		protected string value;
+		
+		public void Input(string path)
+		{
+			try
+			{
+				StreamReader fileReader = new StreamReader(path);
+				value = fileReader.ReadToEnd();
+				fileReader.Close();
+			}
+			catch (System.Exception ex) 
+			{
+				Console.WriteLine("ファイルが読み取れません。" + ex);
+			}
+		}
+		
+		public void Output(string path)
+		{
+			StreamWriter fileWriter = new StreamWriter(path,false);
+			fileWriter.Write(value);
+			fileWriter.Close();
+			
+		}
+	}
+}
