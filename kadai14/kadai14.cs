007be13bc1fcd8524fc4ad661ca821d40a264612
Kei Kawaguchi <k.kawaguchi@cisys.co.jp>
2015-06-23 21:18:24 +0900

    課題14プログラムの概要を記載

---
+++ kadai14/kadai14.cs
@@ -0,0 +1,37 @@
+using System; 
+using System.IO; 
+
+namespace Kadai
+{
+//カレンダー作成
+
+	class Kadai14
+	{
+		//引数の入力、クラスの作成
+		public static void Main()
+		{
+			//年月を受け取る
+			//calendarクラスに年月を渡して一日の曜日と日数を取得
+			//OutPutCalendarクラスに一日の曜日と日数を渡して出力
+		}
+		
+	}
+	//年,月から一日の曜日と最後の日を返す
+	class Calendar 
+	{
+		string month;
+		string year;
+		
+		
+		public void SetMonth(value As string)
+		{
+			month = value;
+		}
+		
+		public void SetYear(value As string)
+		{
+			month = value;
+		}
+	}
+	//出力
+}
