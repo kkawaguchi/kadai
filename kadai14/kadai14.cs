4589ce55c466369d3e2cd83ee693b93bc3b12527
Kei Kawaguchi <k.kawaguchi@cisys.co.jp>
2015-06-27 18:21:33 +0900

    課題14途中まで作成ましした

--- kadai14/kadai14.cs
+++ kadai14/kadai14.cs
@@ -1,5 +1,4 @@
 using System; 
-using System.IO; 
 
 namespace Kadai
 {
@@ -11,27 +10,185 @@ namespace Kadai
 		public static void Main()
 		{
 			//年月を受け取る
+			Console.WriteLine("年と月を入力してください(例：2015,7)");
+			string readYearMonth = Console.ReadLine();
 			//calendarクラスに年月を渡して一日の曜日と日数を取得
-			//OutPutCalendarクラスに一日の曜日と日数を渡して出力
+			Calendar calendar = new Calendar();
+			if (calendar.CheckValue(readYearMonth))
+			{
+				calendar.SetYoubi();
+				calendar.SetNisu();
+				//OutPutCalendarクラスに一日の曜日と日数を渡して出力
+				OutPutCalendar outPutcalendar = new OutPutCalendar(calendar);
+				outPutcalendar.OutPut();
+			}
+			else
+			{
+				return;
+			}
 		}
 		
 	}
 	//年,月から一日の曜日と最後の日を返す
 	class Calendar 
 	{
-		string month;
-		string year;
+		protected int month;
+		protected int year;
+		protected int nisu;
+		protected int youbi;
 		
-		
-		public void SetMonth(value As string)
+		public int GetMonth()
 		{
-			month = value;
+			return month;
 		}
 		
-		public void SetYear(value As string)
+		public int GetYear()
 		{
-			month = value;
+			return year;
+		}
+		
+		public int GetNisu()
+		{
+			return nisu;
+		}
+		
+		public int GetYoubi()
+		{
+			return youbi;
+		}
+		
+		public bool CheckValue(string monthYear)
+		{
+			string getMonth;
+			string getYear;
+			bool checkFlg;
+			checkFlg = true;
+			//チェックしてmonthとyearをセットする
+			
+			if (monthYear.IndexOf(",") == -1)
+			{
+				Console.WriteLine("入力のフォーマットがおかしいです(「,」がありません)");
+				checkFlg = false;
+			}
+			else
+			{
+				string[] listMonthYear  = monthYear.Split(',');
+				if(listMonthYear.Length != 2)
+				{
+					Console.WriteLine("入力のフォーマットがおかしいです(「,」の数がおかしいです)");
+					checkFlg = false;
+				}
+				else
+				{
+					getYear = listMonthYear[0];
+					getMonth = listMonthYear[1];
+					if(!int.TryParse(getYear,out year))
+					{
+						Console.WriteLine("年が数字ではありません");
+						checkFlg = false;
+					}
+					else
+					{
+						if(year < 1 || year > 9999)
+						{
+							Console.WriteLine("年が1～9999ではありません");
+							checkFlg = false;
+						}
+					}
+					if(!int.TryParse(getMonth,out month))
+					{
+						Console.WriteLine("月が数字ではありません");
+						checkFlg = false;
+					}
+					else
+					{
+						if(month < 1 || month > 12)
+						{
+							Console.WriteLine("月が1～12ではありません");
+							checkFlg = false;
+						}
+					}
+				}
+			}
+		return checkFlg;
+		}
+		
+		//一日の曜日を出す
+		public void SetYoubi()
+		{
+			DateTime day = new DateTime(year, month, 1, 00, 00, 00, 000);
+			DayOfWeek week = day.DayOfWeek;
+			switch (week)
+			{
+				case DayOfWeek.Sunday:
+					youbi=0;
+					break;
+				case DayOfWeek.Monday:
+					youbi=1;
+					break;
+				case DayOfWeek.Tuesday:
+					youbi=2;
+					break;
+				case DayOfWeek.Wednesday:
+					youbi=3;
+					break;
+				case DayOfWeek.Thursday:
+					youbi=4;
+					break;
+				case DayOfWeek.Friday:
+					youbi=5;
+					break;
+				case DayOfWeek.Saturday:
+					youbi=6;
+					break;
+			}
+		}
+		//日数を出す
+		public void SetNisu()
+		{
+			nisu = DateTime.DaysInMonth(year, month);
 		}
 	}
 	//出力
+	class OutPutCalendar
+	{
+		Calendar calendar;
+		//コンストラクタ
+		public OutPutCalendar(Calendar value)
+		{
+			calendar = value;
+		}
+		public void OutPut()
+		{
+			string[] outDate = new string[42];
+			
+			if(calendar.GetYoubi() == 0)
+			{
+			}
+			else
+			{
+				for(int i=0;i<GetYoubi();i++)
+				{
+					string[i] = "--";
+				}
+				for(int i=0;i<GetNisu();i++)
+				{
+					
+				}
+			}
+			Console.WriteLine("日　月　火　水　木　金　土");
+			for(int i=0; i<42;i++)
+			{
+				if((i+1)%7==0)
+				{
+					Console.WriteLine(i);
+				}
+				else
+				{
+					Console.Write(i);
+					Console.Write("　");
+				}
+			}
+		}
+	}
 }
